ff20448eac4b92f0623dd80899ee95843c757270
Fabian Linzberger <fabian@klarna.com>
2011-11-10 13:35:55 +0100

    install management plugin via new rabbitmq 2.7 rabbitmq-plugins util

--- rabbitmq/recipes/management.rb
+++ rabbitmq/recipes/management.rb
@@ -1,24 +1,8 @@
 # install the management / monitoring web console
-#
-# currently very much hardcoding that we expect version 2.6.1
 
-plugins = %w{
-  rabbitmq_management-2.6.1.ez
-  rabbitmq_management_agent-2.6.1.ez
-  rabbitmq_mochiweb-2.6.1.ez
-  mochiweb-1.3-rmq2.6.1-git9a53dbd.ez
-  webmachine-1.7.0-rmq2.6.1-hg0c4b60a.ez
-  amqp_client-2.6.1.ez
-}
-
-plugin_dir = "/usr/lib/rabbitmq/lib/rabbitmq_server-2.6.1/plugins/"
-url_prefix = "http://www.rabbitmq.com/releases/plugins/v2.6.1/"
-plugins.each do |plugin|
-  execute "wget " + url_prefix + plugin do
-    cwd plugin_dir
-    creates plugin_dir + plugin
-    notifies :restart, resources(:service => "rabbitmq-server")
-  end
+execute "rabbitmq-plugins enable rabbitmq_management" do
+  not_if "rabbitmq-plugins list rabbitmq_management|grep '\\[E\\] rabbitmq_management'"
+  notifies :restart, resources(:service => "rabbitmq-server")
 end
 
 rabbitmq_user "guest" do
